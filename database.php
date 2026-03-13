0bab31feb2669f75dbd68e0f463d3137f6e243c1
Sampati Anvekar <sampatianvekar@gmail.com>
2026-03-13 11:57:46 +0530

    Update DB connection for Railway

--- database.php
+++ database.php
@@ -1,14 +1,12 @@
 <?php
 
-$host = "localhost";
-$dbname = "login_db";
+$host = "mysql.railway.internal";
+$dbname = "railway";
 $username = "root";
-$password = "";
+$password = "xaoGcrnaOwkVqKoKWGRscGwrxYnpPhTE";
+$port = 3306;
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
+$mysqli = new mysqli($host, $username, $password, $dbname, $port);
                      
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
